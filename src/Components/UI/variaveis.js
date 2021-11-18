66396cc6123b2b5f3aa2b4fa19a8f28e04f56eb8
Alexandra <bguevaraale@gmail.com>
2021-11-18 13:04:46 -0300

    Adicionar temas, uso do ThemeProvider e acrescentar  Switcher no topo

--- src/Components/UI/variaveis.js
+++ src/Components/UI/variaveis.js
@@ -1,3 +1,7 @@
 export const corPrimaria = "#b92bb7";
 export const fundoClaro = "#f1f1f1";
+export const conteudoClaro= "white";
 export const textoFundoClaro = "#grey";
+export const fundoEscuro= "#363537";
+export const conteudoEscuro= "#5c5b5e";
+export const textoFundoEscuro= "#FAFAFA"
